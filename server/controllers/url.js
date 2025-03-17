951a35504c38c9492a19c8d49458322626ab6452
Krishna0723 <purnasaikrishnainnamuri23@gmail.com>
2025-03-18 00:12:34 +0530

    removed log

--- server/controllers/url.js
+++ server/controllers/url.js
@@ -4,7 +4,7 @@ const shortid = require("shortid");
 async function generateShortURL(req, res) {
   const shortId = shortid();
   const ORG_URL = req.body;
-  console.log(ORG_URL);
+  // console.log(ORG_URL);
 
   if (!ORG_URL.url) return res.status(400).json({ error: "url is requires" });
   await URL.create({
